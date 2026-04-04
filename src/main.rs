8e71dc2f83a3fa2685e17cacbd05a2399fb1ad41
Dmytro Svitkin <77505900+Dmytro-Svitkin@users.noreply.github.com>
2026-04-05 01:01:30 +0200

    fn from_base_10 finished, several fns renamed

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
-const WAR:&str="~WAR~";
+fn war()->String{
+    String::from("~WAR~")
+}
 
 fn main() {
-    println!("{}",base(16));
-    let e:Result<usize, String>=x_to_base_10(&mut String::from("1A"),&mut base(16));
-    { println!("{:?}",e)};
+    println!("{}",convert(&mut String::from("a"),&mut String::from("a"),&mut String::from("a")));
 }
 
 fn base(x:usize)->String{
     let base_62:&str="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if x>0&&x<63{return base_62[..x].to_string()};
-    WAR.to_string()
+    war()
 }
 
-fn x_to_base_10(x:&mut String,in_base:&mut String)->Result<usize,String>{
+fn to_base_10(x:&mut String,in_base:&mut String)->Result<usize,String>{
     let mut result:usize=0;
     let mut pwr:u32=0;
     let in_base_len:usize=in_base.len();
     for n in x.chars().rev(){
         let dig:usize=match in_base.chars().position(|c| c == n){
-            Some(i)=>{i},_=>{return Err(WAR.to_string())}
+            Some(i)=>{i},_=>{return Err(war())}
         };
         result+=dig*(in_base_len.pow(pwr));pwr+=1;
     };
     return Ok(result);
+}
+
+fn from_base_10(mut x:usize,out_base:&mut String)->String{
+    let mut result:String=String::new();
+    let out_base_len:usize=out_base.len();
+    if x==0{return out_base.chars().nth(0).unwrap().to_string()}
+    while x>0{
+        result.push(out_base.chars().nth(x%out_base_len).unwrap());
+        x/=out_base_len;
+    }
+    return result.chars().rev().collect();
+}
+
+fn convert(x:&mut String,in_base:&mut String,out_base:&mut String)->String{
+
+    war()
 }
