f5abdc3dd8edb66662e125ddfba82ac726b53b34
Dmytro Svitkin <77505900+Dmytro-Svitkin@users.noreply.github.com>
2026-04-05 02:33:50 +0200

    major changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
+use std::{io::{self, Write}};
+
 fn war()->String{
     String::from("~WAR~")
 }
 
+fn input(msg:&str)->String{
+    print!("{}",msg);
+    io::stdout().flush().unwrap();
+    let mut result:String=String::new();
+    io::stdin().read_line(&mut result).unwrap();
+    result
+}
+
 fn main() {
-    println!("{}",convert(&mut String::from("a"),&mut String::from("a"),&mut String::from("a")));
+    loop {
+        let mut a:String=input("Value: ").trim().to_string();
+        let mut b:String=input("Input base: ").trim().to_string();
+        let mut c:String=input("Output base: ").trim().to_string();
+
+        if b.chars().all(|ch|ch.is_ascii_digit()){
+            let recol:usize=b.parse::<usize>().unwrap();
+            if recol>62{println!("~WAR~ Bases cannot exceed 62, unless it is a custom base.")}
+            else if recol<1{println!("~WAR~ Bases cannot stay below 1.")}
+            else{b=base(recol)}
+        };
+
+        if c.chars().all(|ch|ch.is_ascii_digit()){
+            let recol:usize=c.parse::<usize>().unwrap();
+            if recol>62{println!("~WAR~ Bases cannot exceed 62, unless it is a custom base.")}
+            else if recol<1{println!("~WAR~ Bases cannot stay below 1.")}
+            else{c=base(recol)}
+        };
+
+        println!("{}\n",convert(&mut a,&mut b,&mut c))
+    }
 }
 
 fn base(x:usize)->String{
@@ -13,11 +43,12 @@ fn base(x:usize)->String{
 }
 
 fn to_base_10(x:&mut String,in_base:&mut String)->Result<usize,String>{
+    let in_base_len:usize=in_base.len();
+    if in_base_len==1{return Ok(x.len())};
     let mut result:usize=0;
     let mut pwr:u32=0;
-    let in_base_len:usize=in_base.len();
     for n in x.chars().rev(){
-        let dig:usize=match in_base.chars().position(|c| c == n){
+        let dig:usize=match in_base.chars().position(|c|c==n){
             Some(i)=>{i},_=>{return Err(war())}
         };
         result+=dig*(in_base_len.pow(pwr));pwr+=1;
@@ -37,6 +68,11 @@ fn from_base_10(mut x:usize,out_base:&mut String)->String{
 }
 
 fn convert(x:&mut String,in_base:&mut String,out_base:&mut String)->String{
-
+    if in_base!=&mut war()&&out_base!=&mut war(){
+        let local_result:Result<usize,String>=to_base_10(x, in_base);
+        if local_result!=Err(war()){
+            return from_base_10(local_result.unwrap(), out_base);
+        }
+    }
     war()
 }
